0ab536f9a3a1f6dfc0be83e485a530a891dff6fc
whereisrahul <www.rahulreddy@gmail.com>
2025-06-08 22:32:41 +0530

    Update index.js
    update bio

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,15 @@ export default function Home() {
       </Head>
       <main>
         <h1>Welcome to Where's Rahul?</h1>
-        <p>Explore Moto Vlogs, Travel Diaries, Food Trips & More</p>
+        <p>Welcome to the journey! I’m Rahul – always on the move, always exploring, and always sharing real-life moments as they unfold. From everyday vibes to unexpected adventures, this channel is your front-row seat to lifestyle, travel, food, routines, and everything in between.
+
+Expect chill hangouts, fun discoveries, and maybe a little chaos too.
+Will try posting new videos every week – so hit subscribe and let’s go find out... Where’s Rahul?</p>
         <ul>
-          <li><a href="https://youtu.be/VtonaDDruP8?si=h2agHJR8SUFVAoRt">Trip to Mysore</a></li>
-          <li><a href="https://youtu.be/uuX4aC9Czl8?si=Cz2nVBDiN3Rzx7R-">Sunset Rides</a></li>
-          <li><a href="https://youtu.be/XNtJwhbFUj0?si=4y2NK9341gSDGAgF">Monsoon Trails</a></li>
-          <li><a href="https://youtu.be/A3jBRZxDllE?si=Td1WNhGFug4-KzI_">Temple Route</a></li>
+          <li><a href="https://youtu.be/VtonaDDruP8?si=h2agHJR8SUFVAoRt">Hyderabad to Europe... But First, a Dubai Layover!</a></li>
+          <li><a href="https://youtu.be/uuX4aC9Czl8?si=Cz2nVBDiN3Rzx7R-">A Mini Travel Film: Dubai to Schiphol Airport, Netherlands </a></li>
+          <li><a href="https://youtu.be/XNtJwhbFUj0?si=4y2NK9341gSDGAgF">Entering into Utrecht from Amsterdam</a></li>
+          <li><a href="https://youtu.be/A3jBRZxDllE?si=Td1WNhGFug4-KzI_">Evening Walk in Utrecht</a></li>
         </ul>
       </main>
     </>
